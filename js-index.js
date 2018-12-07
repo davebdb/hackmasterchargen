fee1614697aa36634e053c7c6f2ad62e418876d0
David Bukowski <davebdb@users.noreply.github.com>
2018-12-06 22:38:05 -0600

    split into HTML, CSS, JS files

---
+++ js-index.js
@@ -0,0 +1,59 @@
+var attributesArray = ["str","int","wis","dex","con","lks","cha"];
+var attributesArrayLength = attributesArray.length;	
+var attributesValues = [];
+
+//STRENGTH object
+var strength = new Object();
+
+//INTELLIGENCE object
+var intelligence = new Object();
+
+//WISDOM object
+var wisdom = new Object();
+
+//DEXTERITY object
+var dexterity = new Object();
+
+//CONSTITUTION object
+var constitution = new Object();
+
+//LOOKS object
+var looks = new Object();
+
+//CHARISMA object
+var charisma = new Object();
+
+function rollAbilityScore() {
+	var rollNumber;
+	var diceRoll;
+	var d100;
+	var totalRoll=0;
+		for (rollNumber=0;rollNumber<3;rollNumber++) {
+		diceRoll = Math.floor(Math.random()*6)+1;
+		totalRoll +=diceRoll;
+	}
+d100=Math.floor(Math.random()*100)*0.01;
+totalRoll +=d100;
+return totalRoll;
+}
+
+strength.value=rollAbilityScore();
+intelligence.value=rollAbilityScore();
+wisdom.value=rollAbilityScore();
+dexterity.value=rollAbilityScore();
+constitution.value=rollAbilityScore();
+looks.value=rollAbilityScore();
+charisma.value=rollAbilityScore();
+
+document.getElementById("strength").textContent=strength.value;
+document.getElementById("intelligence").textContent=intelligence.value;
+document.getElementById("wisdom").textContent=wisdom.value;
+document.getElementById("dexterity").textContent=dexterity.value;
+document.getElementById("constitution").textContent=constitution.value;
+document.getElementById("looks").textContent=looks.value;
+document.getElementById("charisma").textContent=charisma.value;
+
+//document.getElementById(attributesArray[counter]).textContent=attributesArray[counter] + ":" + attribValue.toFixed(2);
+//document.write("<br>"+attributesArray[counter]+":"+totalRoll+"."+d100+"<br><br><br>");
+
+	
